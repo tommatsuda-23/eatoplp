05e5a3608040c9a06c4be143514981b22a5f5fdc
you-5805 <you.iwamoto918@gmail.com>
2020-11-08 19:39:07 +0900

    '1108'

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,33 +1,55 @@
 <template>
   <div>
-    <div id="mv">
-      <div id="mv-content">
-        <div id="icon">
-          <img src="../assets/images/icon.png" alt="アイコン">
+    <template name="fade">
+      <div id="mv">
+        <div id="mv-content">
+          <div id="icon">
+            <img src="../assets/images/icon.png" alt="アイコン">
+          </div>
+          <img src="../assets/images/copy.png" alt="食べるという、コミュニケーション" id="copy">
+          <h2 id="desc">「まち」の食育プロジェクト<br>EATOP</h2>
         </div>
-        <img src="../assets/images/copy.png" alt="食べるという、コミュニケーション" id="copy">
-        <h2 id="desc">「まち」の食育プロジェクト<br>EATOP</h2>
       </div>
-    </div>
+    </template>
     <div id="contents-container">
-      <topAbout/>
-      <topEvent/>
-      <topLogs/>
+      <section id="about">
+        <div class="container">
+          <h2>共に作り、共に食べる</h2>
+          <div id="description">
+            <p>EATOP（いーとっぷ）は、大学生、留学生、地域の方々を対象として、<br>「社会性」「芸術性」「国際性」を重視した食育活動を行っています。
+            <br>ここでいう食育は、単なる食事ではありません。
+            <br><span>「共に作り、共に食べる」</span>という形こそ、<br>本来の食育の形です。</p>
+          </div>
+          <button @click="toAbout" class="btn">もっと詳しく</button>
+        </div>
+      </section>
+      <section id="event">
+        <div class="container">
+          <button @click="toEvent" class="btn">もっと詳しく</button>
+        </div>
+      </section>
+      <section id="logs">
+        <div class="container">
+          <button @click="toLogs" class="btn">もっと詳しく</button>
+        </div>
+      </section>
     </div>
   </div>
 </template>
 
 <script>
-import topAbout from '@/components/topAbout'
-import topEvent from '@/components/topEvent'
-import topLogs from '@/components/topLogs'
-
 export default {
   name: 'top',
-  components: {
-    topAbout: topAbout,
-    topEvent: topEvent,
-    topLogs: topLogs
+  methods: {
+    toAbout () {
+      this.$router.push('about')
+    },
+    toEvent () {
+      this.$router.push('event')
+    },
+    toLogs () {
+      this.$router.push('logs')
+    }
   }
 }
 </script>
@@ -75,6 +97,38 @@ export default {
     font-weight: normal;
     margin: 20px 0;
     color:  rgba(0, 0, 0, 0.692);
+}
+
+.container {
+  margin: 50px 30px;
+}
+
+#about h2 {
+  font-size: 30px;
+  margin-bottom: 30px;
+}
+
+#about #description {
+  color: rgba(0, 0, 0, 0.8);
+  font-size: 20px;
+  line-height: 40px;
+}
+
+.btn {
+  background-color: #ffc165;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  margin: 20px 0;
+  width: 251px;
+  height: 50px;
+  border: none;
+  border-radius: 25px;
+  cursor: pointer;
+  font-size: 20px;
+  color: white;
+}
+
+#event {
+  display: none;
 }
 
 @media (min-width: 1400px) {
