eae686b29ed37c774ac76dc145a6b55561d43ba0
you-5805 <you.iwamoto918@gmail.com>
2020-11-11 14:10:31 +0900

    'sass、できなかったのでまた後でやります'

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router/router'
 
-require('@/assets/sass/main.scss')
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
