e4adc8991869387187ad5d0cef754d7e7c5ac35c
you-5805 <you.iwamoto918@gmail.com>
2020-11-15 17:32:17 +0900

    about template修正

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,70 +3,72 @@
     <transition name="fade">
       <div id="about-mv"></div>
     </transition>
-    <div>
-     <br><br>
-     <h1 id="about-title">共に作り、共に食べる</h1>
-     <div id="description">
-       <h3>EATOPの目的</h3>
-       <p>ライフスタイルの急激な変化で<br>かつての「誰かとご飯を食べる」という＜共食＞の概念はくずれ<br>一人でご飯を食べる＜孤食＞が広まりつつあります。
-       <br>本来食事の場は、たくさんの会話が交わされる、感情にあふれた、
-       <br>互いに学び合える場所でした。
-       <br>そうした場所が失われそうな今だからこそ、もう一度コミュニケーションとしての＜共食＞を大切にしたい。
-       <br>そんな思いで、2018年6月から、この活動は始まりました。
-       <br>この活動で目指しているのは、「共に作り、共に食べる」ができる、環境作り。<br>では、このEATOPの活動で、誰と誰が「共に作り、食べる」環境を作るのか。</p>
-       <br>
-       <h3>登場人物は3人です。</h3>
-       <p>1人目は、この土地の「食の魅力」をしらない大学生。<br>2人目は、学生とは、繋がりがなかなか生まれない地域の方。<br>3人目は、九州大学（日本）に来てくれた留学生。<br>この三者が「食べる」というコミュニケーションを通して、紡ぎ出す物語を楽しみに活動しています。</p>
-       <br>
-       <p>「糸島」は豊かな食の宝庫。<br>この場所にあるものの「価値」を存分に生かして。<br>普段あまり交わらないもの同士。<br>美味しい食事を、みんなで美しく作って、食べる。</p>
-     </div>
-     <div id="tanaka">
-      <div id="tanaka-left">
-        <h2>代表</h2>
-        <h1>田中  稀ひろ<span> KIHIRO TANAKA</span></h1>
-        <p>{{ kingo }}</p>
+    <section id="introduction">
+      <h1 id="about-title">共に作り、共に食べる</h1>
+      <div id="description">
+        <h3>EATOPの目的</h3>
+        <p>ライフスタイルの急激な変化で<br>かつての「誰かとご飯を食べる」という＜共食＞の概念はくずれ、
+        <br>一人でご飯を食べる＜孤食＞が広まりつつあります。
+        <br>本来食事の場は、たくさんの会話が交わされる、
+        <br>感情にあふれた、互いに学び合える場所でした。
+        <br>そうした場所が失われそうな今だからこそ、
+        <br>もう一度コミュニケーションとしての＜共食＞を大切にしたい。
+        <br>そんな思いで、2018年6月から、この活動は始まりました。<br>
+        <br>この活動で目指しているのは、「共に作り、共に食べる」ができる、環境作り。<br>では、このEATOPの活動で、誰と誰が「共に作り、食べる」環境を作るのか。</p>
+        <br>
+        <h3>登場人物は3人です。</h3>
+        <p>1人目は、この土地の「食の魅力」をしらない大学生。<br>2人目は、学生とは、繋がりがなかなか生まれない地域の方。<br>3人目は、九州大学（日本）に来てくれた留学生。<br>この三者が、
+        <br>「食べる」というコミュニケーションを通して紡ぎ出す物語を楽しみに
+        <br>EATOPは活動しています。</p>
+        <br>
+        <p>「糸島」は豊かな食の宝庫。<br>この場所にあるものの「価値」を存分に生かして。<br>普段あまり交わらないもの同士。<br>美味しい食事を、みんなで美しく作って、食べる。</p>
       </div>
-      <div id="tanaka-right">
-         <img src="../assets/images/about/tanaka.png" alt="田中 稀ひろ">
+    </section>
+    <section id="members">
+      <div id="tanaka">
+        <div id="tanaka-left">
+          <h2>代表</h2>
+          <h1>田中  稀ひろ<span> KIHIRO TANAKA</span></h1>
+          <p>{{ kingo }}</p>
+        </div>
+        <div id="tanaka-right">
+          <img src="../assets/images/about/tanaka.png" alt="田中 稀ひろ">
+        </div>
       </div>
-     </div>
-     <div id="sasazu">
-      <div id="sasazu-left">
-         <img src="../assets/images/about/sasazu.png" alt="笹津 敏靖">
+      <div id="sasazu">
+        <div id="sasazu-left">
+          <img src="../assets/images/about/sasazu.png" alt="笹津 敏靖">
+        </div>
+        <div id="sasazu-right">
+          <h2>創設者</h2>
+          <h1>笹津  敏靖<span> TOSHINOBU SASAZU</span></h1>
+          <p>{{ kingo }}</p>
+        </div>
       </div>
-      <div id="sasazu-right">
-        <h2>創設者</h2>
-        <h1>笹津  敏靖<span> TOSHINOBU SASAZU</span></h1>
-        <p>{{ kingo }}</p>
+    </section>
+    <section>
+      <h1 id="title">参加者の声</h1>
+      <div id="voice">
+        <div class="participant">
+          <img src="../assets/images/about/participant1.png" alt="地域の方々">
+          <h2>地域の方々</h2>
+          <p>{{ kingo }}</p>
+        </div>
+        <div class="participant">
+          <img src="../assets/images/about/participant2.png" alt="地域の方々">
+          <h2>大学生</h2>
+          <p>{{ kingo }}</p>
+        </div>
+        <div class="participant">
+          <img src="../assets/images/about/participant3.png" alt="地域の方々">
+          <h2>留学生</h2>
+          <p>{{ kingo }}</p>
+        </div>
       </div>
-     </div>
-     <div>
-       <h1 id="title">参加者の声</h1>
-     </div>
-     <div id="voice">
-       <div class="participant">
-        <img src="../assets/images/about/participant1.png" alt="地域の方々">
-        <h2>地域の方々</h2>
-        <p>{{ kingo }}</p>
-       </div>
-       <div class="participant">
-        <img src="../assets/images/about/participant2.png" alt="地域の方々">
-        <h2>大学生</h2>
-        <p>{{ kingo }}</p>
-       </div>
-       <div class="participant">
-        <img src="../assets/images/about/participant3.png" alt="地域の方々">
-        <h2>留学生</h2>
-        <p>{{ kingo }}</p>
-       </div>
-     </div>
-     <div>
-       <h1 id="title">パートナー</h1>
-     </div>
-     <div id="partner">
-       <p>{{ kingo }}</p>
-     </div>
-    </div>
+    </section>
+    <section>
+      <h2>パートナー</h2>
+    </section>
   </div>
 </template>
 
@@ -115,6 +117,10 @@ body {
     #about-mv {
         height: 650px;
     }
+}
+
+#introduction {
+  margin: 40px 0 70px;
 }
 
 #about-title {
--- src/views/Logs.vue
+++ src/views/Logs.vue
@@ -3,7 +3,7 @@
     <transition name="fade" mode="out-in">
       <div id="map">
         <template v-for="(prop, key) in pinProps">
-          <router-link :key="key" :to="route">
+          <router-link :key="key" to="">
             <Pin :key="key" v-bind="{ imgSrc: prop['imgSrc'], position: prop['position'], imgAlt: prop['imgAlt']}"/>
           </router-link>
         </template>
@@ -23,16 +23,16 @@ export default {
   data () {
     return {
       pinProps: [
-        {imgSrc: require('../assets/images/logs/pins/gurugura.png'), position: [80, 51], imgAlt: '糸島グルメグランプリ', link: 'gurugura'},
-        {imgSrc: require('../assets/images/logs/pins/hiho.png'), position: [20, 20], imgAlt: 'ハイホー', link: 'hiho'},
-        {imgSrc: require('../assets/images/logs/pins/imari.png'), position: [40, 30], imgAlt: '伊万里田植え体験', link: 'imari'},
-        {imgSrc: require('../assets/images/logs/pins/kyudaisai.png'), position: [40, 50], imgAlt: '九大祭', link: 'kyudaisai'},
-        {imgSrc: require('../assets/images/logs/pins/pietro.png'), position: [50, 80], imgAlt: 'ピエトロ', link: 'pietro'},
-        {imgSrc: require('../assets/images/logs/pins/qshock.png'), position: [20, 50], imgAlt: 'Q-SHOCK', link: 'qshock'},
-        {imgSrc: require('../assets/images/logs/pins/sabameshi.png'), position: [10, 60], imgAlt: 'サバイバル飯キャンプ', link: 'sabameshi'},
-        {imgSrc: require('../assets/images/logs/pins/saito.png'), position: [40, 40], imgAlt: 'さいとぴあバレンタイン', link: 'saito'},
-        {imgSrc: require('../assets/images/logs/pins/saitorensai.png'), position: [80, 90], imgAlt: '西都連祭', link: 'saitorensai'},
-        {imgSrc: require('../assets/images/logs/pins/tofu.png'), position: [50, 10], imgAlt: 'またいちの塩で豆腐作り', link: 'tofu'}
+        {imgSrc: require('../assets/images/logs/pins/gurugura.png'), position: [30, 35], imgAlt: '糸島グルメグランプリ', link: ''},
+        {imgSrc: require('../assets/images/logs/pins/hiho.png'), position: [39, 57], imgAlt: 'ハイホー', link: ''},
+        {imgSrc: require('../assets/images/logs/pins/imari.png'), position: [8, 25], imgAlt: '伊万里田植え体験', link: ''},
+        {imgSrc: require('../assets/images/logs/pins/kyudaisai.png'), position: [50, 24], imgAlt: '九大祭', link: ''},
+        {imgSrc: require('../assets/images/logs/pins/pietro.png'), position: [86, 65], imgAlt: 'ピエトロ', link: ''},
+        {imgSrc: require('../assets/images/logs/pins/qshock.png'), position: [42, 60], imgAlt: 'Q-SHOCK', link: ''},
+        {imgSrc: require('../assets/images/logs/pins/sabameshi.png'), position: [50, 50], imgAlt: 'サバイバル飯キャンプ', link: ''},
+        {imgSrc: require('../assets/images/logs/pins/saito.png'), position: [68, 47], imgAlt: 'さいとぴあバレンタイン', link: ''},
+        {imgSrc: require('../assets/images/logs/pins/saitorensai.png'), position: [66, 43], imgAlt: '西都連祭', link: ''},
+        {imgSrc: require('../assets/images/logs/pins/tofu.png'), position: [53, 18], imgAlt: 'またいちの塩で豆腐作り', link: ''}
       ]
     }
   },
